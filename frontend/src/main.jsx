2d71a23cf3e112b93fb27479a4e2cb48bb4df1aa
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2024-08-19 22:03:27 +0300

    edited main.jsx final

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,31 +1,17 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./css/index.css";
-// import { browserRoutes } from "./constants/Routing.jsx";
-// import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// import { Flowbite } from "flowbite-react";
-// import theme from "./assets/FlowbiteTheme.jsx";
-import App from "./App.jsx";
+import { browserRoutes } from "./constants/Routing.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Flowbite } from "flowbite-react";
+import theme from "./assets/FlowbiteTheme.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <Flowbite theme={{ theme: theme }}>
+            <RouterProvider
+                router={createBrowserRouter(browserRoutes)}
+            ></RouterProvider>
+        </Flowbite>
     </StrictMode>
 );
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-// <React.StrictMode>
-// <Flowbite theme={{ theme: theme }}>
-// <div className="font-bold text-2xl text-center">Main Content</div>
-{
-    /* <RouterProvider */
-}
-{
-    /* // router={createBrowserRouter(browserRoutes)} */
-}
-{
-    /* ></RouterProvider> */
-}
-// </Flowbite>
-// </React.StrictMode>
-// );
