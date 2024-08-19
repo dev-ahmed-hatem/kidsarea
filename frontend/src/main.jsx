bee5611ddab40497c03ccfeb32d99f6b8f21d239
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2024-08-19 22:00:59 +0300

    edited index.html and main.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
 import "./css/index.css";
-import { browserRoutes } from "./constants/Routing.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Flowbite } from "flowbite-react";
-import theme from "./assets/FlowbiteTheme.jsx";
+// import { browserRoutes } from "./constants/Routing.jsx";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import { Flowbite } from "flowbite-react";
+// import theme from "./assets/FlowbiteTheme.jsx";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Flowbite theme={{ theme: theme }}>
-            <div className="font-bold text-2xl text-center">Main Content</div>
-            {/* <RouterProvider */}
-                {/* // router={createBrowserRouter(browserRoutes)} */}
-            {/* ></RouterProvider> */}
-        </Flowbite>
-    </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+// <React.StrictMode>
+// <Flowbite theme={{ theme: theme }}>
+// <div className="font-bold text-2xl text-center">Main Content</div>
+{
+    /* <RouterProvider */
+}
+{
+    /* // router={createBrowserRouter(browserRoutes)} */
+}
+{
+    /* ></RouterProvider> */
+}
+// </Flowbite>
+// </React.StrictMode>
+// );
